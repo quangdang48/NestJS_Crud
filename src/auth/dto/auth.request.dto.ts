2a9e2ef130ffb2063203781a21f6d6687fda87b8
HaDangQuang <hadangquang1408@gmail.com>
2025-11-06 17:20:14 +0700

    first commit

---
+++ src/auth/dto/auth.request.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  password: string;
+}
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  password: string;
+  @IsNotEmpty()
+  firstName: string;
+  @IsNotEmpty()
+  lastName: string;
+}
